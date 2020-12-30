37ebc70d84a5115409b6dffc8627f6aeb3501066
Philippe Langevin <philippe.langevin@gmail.com>
2020-12-29 22:15:50 -0500

    Now https calls to json server

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://my-json-server.typicode.com/plangevin/Real-World_Vue-3",
+  baseURL: "https://my-json-server.typicode.com/plangevin/Real-World_Vue-3",
   withCredentials: false,
   headers: {
     Accept: "application/json",
